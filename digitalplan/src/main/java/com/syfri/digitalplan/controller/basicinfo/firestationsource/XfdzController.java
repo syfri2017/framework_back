862b4ab1b7c7b259847a3419a939ca9e8b063bdf
lixue0919 <442019696@qq.com>
2018-08-10 10:10:37 +0800

    所属队站Tree从总队开始，去掉公安部消防局

--- digitalplan/src/main/java/com/syfri/digitalplan/controller/basicinfo/firestationsource/XfdzController.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/controller/basicinfo/firestationsource/XfdzController.java
@@ -82,7 +82,7 @@ public class XfdzController extends BaseController<XfdzVO> {
     public @ResponseBody ResultVO findSjdzByUser(@RequestBody XfdzVO xfdzVO) {
         ResultVO resultVO = ResultVO.build();
         try {
-            resultVO.setResult(xfdzService.doFindDzTreeByUserAll(xfdzVO));
+            resultVO.setResult(xfdzService.doFindDzTreeByUser(xfdzVO));
         } catch (Exception e) {
             logger.error("{}", e.getMessage());
             resultVO.setCode(EConstants.CODE.FAILURE);
