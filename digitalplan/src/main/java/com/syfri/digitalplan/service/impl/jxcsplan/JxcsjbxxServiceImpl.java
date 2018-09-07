f9d3eb4236228ea1933442ce01602f1a6277b29e
rliu666 <rliu666@sina.cn>
2018-09-07 15:32:33 +0800

    九小场所-删除功能

--- digitalplan/src/main/java/com/syfri/digitalplan/service/impl/jxcsplan/JxcsjbxxServiceImpl.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/impl/jxcsplan/JxcsjbxxServiceImpl.java
@@ -9,15 +9,25 @@ import com.syfri.digitalplan.model.jxcsplan.JxcsjbxxVO;
 import com.syfri.digitalplan.service.jxcsplan.JxcsjbxxService;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional(rollbackFor = {Exception.class, RuntimeException.class})
 @Service("jxcsjbxxService")
 public class JxcsjbxxServiceImpl extends BaseServiceImpl<JxcsjbxxVO> implements JxcsjbxxService {
 
-	@Autowired
-	private JxcsjbxxDAO jxcsjbxxDAO;
+    @Autowired
+    private JxcsjbxxDAO jxcsjbxxDAO;
 
-	@Override
-	public JxcsjbxxDAO getBaseDAO() {
-		return jxcsjbxxDAO;
-	}
+    @Override
+    public JxcsjbxxDAO getBaseDAO() {
+        return jxcsjbxxDAO;
+    }
+
+    public int doDeleteByVOList(List<JxcsjbxxVO> jxcsjbxxVOList) {
+        int count = 0;
+        for (JxcsjbxxVO vo : jxcsjbxxVOList) {
+            count = count + jxcsjbxxDAO.doUpdateByVO(vo);
+        }
+        return count;
+    }
 }
--- digitalplan/src/main/java/com/syfri/digitalplan/service/jxcsplan/JxcsjbxxService.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/jxcsplan/JxcsjbxxService.java
@@ -3,5 +3,10 @@ package com.syfri.digitalplan.service.jxcsplan;
 import com.syfri.baseapi.service.BaseService;
 import com.syfri.digitalplan.model.jxcsplan.JxcsjbxxVO;
 
-public interface JxcsjbxxService  extends BaseService<JxcsjbxxVO>{
+import java.util.List;
+
+public interface JxcsjbxxService extends BaseService<JxcsjbxxVO> {
+
+    /*--批量删除九小场所 by liurui 2018/9/7--*/
+    int doDeleteByVOList(List<JxcsjbxxVO> jxcsjbxxVOList);
 }
