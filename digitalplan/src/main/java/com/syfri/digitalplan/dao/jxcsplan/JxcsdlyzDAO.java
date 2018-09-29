1bab353c7381ae972d5a9c2065a9e98610c2a380
yushch <1096853079@qq.com>
2018-09-29 13:55:07 +0800

    九小编辑功能

--- digitalplan/src/main/java/com/syfri/digitalplan/dao/jxcsplan/JxcsdlyzDAO.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/dao/jxcsplan/JxcsdlyzDAO.java
@@ -4,4 +4,7 @@ import com.syfri.baseapi.dao.BaseDAO;
 import com.syfri.digitalplan.model.jxcsplan.JxcsdlyzVO;
 
 public interface JxcsdlyzDAO extends BaseDAO<JxcsdlyzVO>{
+    //根据单位id逻辑删除中间表中信息
+    int doDeleteByDwid(String dwid);
+
 }
--- digitalplan/src/main/java/com/syfri/digitalplan/service/impl/jxcsplan/JxcsjbxxServiceImpl.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/impl/jxcsplan/JxcsjbxxServiceImpl.java
@@ -13,6 +13,7 @@ import com.syfri.digitalplan.dao.jxcsplan.JxcsjbxxDAO;
 import com.syfri.digitalplan.model.jxcsplan.JxcsjbxxVO;
 import com.syfri.digitalplan.service.jxcsplan.JxcsjbxxService;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,7 +44,7 @@ public class JxcsjbxxServiceImpl extends BaseServiceImpl<JxcsjbxxVO> implements 
     }
 
     //新增九小场所 add by yushch 20180920
-    public JxcsjbxxVO doInsertJbcsByVO(JxcsjbxxVO vo) {
+    public JxcsjbxxVO doInsertJxcsByVO(JxcsjbxxVO vo) {
         jxcsjbxxDAO.doInsertByVO(vo);
         if(vo.getJzxxList().size() != 0){
             List<JxcsjzxxVO> jxcsjzxx = vo.getJzxxList();
@@ -60,11 +61,79 @@ public class JxcsjbxxServiceImpl extends BaseServiceImpl<JxcsjbxxVO> implements 
             List<JxcsxfssVO> jxcsxfss = vo.getXfssList();
             for(int i=0;i<jxcsxfss.size();i++){
                 jxcsxfss.get(i).setDwid(vo.getUuid());
+                jxcsxfss.get(i).setCjrid(vo.getCjrid());
+                jxcsxfss.get(i).setCjrmc(vo.getCjrmc());
+                jxcsxfss.get(i).setJdh(vo.getJdh());
+                jxcsxfss.get(i).setDatasource(vo.getDatasource());
             }
             jxcsxfssDAO.doBatchInsertByList(jxcsxfss);
         }
+        return vo;
+    }
+
+    //编辑九小场所 yushch 20180929
+    public JxcsjbxxVO doUpdateJxcsByVO(JxcsjbxxVO vo){
+        jxcsjbxxDAO.doUpdateByVO(vo);
+        String dwid = vo.getUuid();
+        //根据单位id物理删除中间表中信息
+        jxcsdlyzDAO.doDeleteByDwid(dwid);
+        //重新创建建筑中间表信息
+        if(vo.getJzxxList().size() != 0){
+            List<JxcsjzxxVO> jxcsjzxx = vo.getJzxxList();
+            List<JxcsdlyzVO> jxcsdlyz =new ArrayList<JxcsdlyzVO>();
+            for(int i=0;i<jxcsjzxx.size();i++){
+                JxcsdlyzVO jxcsdlyzVO = new JxcsdlyzVO();
+                jxcsdlyzVO.setJzid(jxcsjzxx.get(i).getJzid());
+                jxcsdlyzVO.setDwid(vo.getUuid());
+                jxcsdlyz.add(jxcsdlyzVO);
+            }
+            jxcsdlyzDAO.doBatchInsertByList(jxcsdlyz);
+        }
+
+    //消防设施
+        //获取新消防设施list
+        List<JxcsxfssVO> xfssList = vo.getXfssList();
+        //获取旧的消防设施list
+        List<JxcsxfssVO> xfssList_old = jxcsxfssDAO.doFindXfssByDwid(dwid);
+        //删除旧的有，新的没有的消防设施
+        for(JxcsxfssVO vo_old : xfssList_old){
+            Boolean flag = true;
+            for(JxcsxfssVO vo_new : xfssList){
+                if(vo_old.getXfssid().equals(vo_new.getXfssid()) && vo_new.getXfssid() != null){
+                    flag = false;
+                }
+            }
+            if(flag){
+                vo_old.setDeleteFlag("Y");
+                vo_old.setXgrid(vo.getXgrid());
+                vo_old.setXgrmc(vo.getXgrmc());
+                jxcsxfssDAO.doUpdateByVO(vo_old);
+            }
+        }
+        //新增或修改消防设施
+        for(JxcsxfssVO vo_new : xfssList){
+            if(!StringUtils.isEmpty(vo_new.getXfssid())){
+                vo_new.setXgrid(vo.getXgrid());
+                vo_new.setXgrmc(vo.getXgrmc());
+                vo_new.setJdh(vo.getJdh());
+                vo_new.setDatasource(vo.getDatasource());
+                jxcsxfssDAO.doUpdateByVO(vo_new);
+            }else {
+                vo_new.setDwid(vo.getUuid());
+                vo_new.setCjrid(vo.getXgrid());
+                vo_new.setCjrmc(vo.getXgrmc());
+                vo_new.setJdh(vo.getJdh());
+                vo_new.setDatasource(vo.getDatasource());
+                /*
+                List<JxcsxfssVO> listNew = new ArrayList<>();
+                listNew.add(vo_new);
+                jxcsxfssDAO.doBatchInsertByList(listNew);
+                */
+                jxcsxfssDAO.doInsertByVO(vo_new);
+            }
+        }
+
 
         return vo;
-
     }
 }
