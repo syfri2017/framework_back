1deb3af88cc13d0983f544ceeb4a89ea5f046d9c
rliu666 <rliu666@sina.cn>
2018-10-09 09:04:00 +0800

    企业介绍文件建立

---
+++ exhibition/src/main/java/com/syfri/exhibition/dao/prediction/QyjsDAO.java
@@ -0,0 +1,7 @@
+package com.syfri.exhibition.dao.prediction;
+
+import com.syfri.baseapi.dao.BaseDAO;
+import com.syfri.exhibition.model.prediction.QyjsVO;
+
+public interface QyjsDAO extends BaseDAO<QyjsVO>{
+}
