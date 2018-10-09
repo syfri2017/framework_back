5f0a620b56b3a8da534d1fadbd3570e3b163f743
rliu666 <rliu666@sina.cn>
2018-10-09 11:10:58 +0800

    营业执照预览

--- exhibition/src/main/java/com/syfri/exhibition/controller/prediction/QyjbxxController.java
+++ exhibition/src/main/java/com/syfri/exhibition/controller/prediction/QyjbxxController.java
@@ -1,9 +1,12 @@
 package com.syfri.exhibition.controller.prediction;
 
+import com.syfri.baseapi.model.ResultVO;
+import com.syfri.baseapi.utils.EConstants;
+import io.swagger.annotations.ApiOperation;
+import com.syfri.exhibition.utils.Base64ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.syfri.exhibition.model.prediction.QyjbxxVO;
 import com.syfri.exhibition.service.prediction.QyjbxxService;
@@ -21,4 +24,25 @@ public class QyjbxxController  extends BaseController<QyjbxxVO>{
 		return this.qyjbxxService;
 	}
 
+	/**
+	 * @Description: 根据企业id获取营业执照
+	 * @Author: rliu
+	 * @Date: 2018/10/9 10:35
+	 */
+	@ApiOperation(value="",notes="")
+	@GetMapping("/doFindYyzzById/{qyid}")
+	public @ResponseBody ResultVO getDetail(@PathVariable String qyid){
+		ResultVO resultVO = ResultVO.build();
+		try{
+			QyjbxxVO result = qyjbxxService.doFindById(qyid);
+			//将二进制转为Base64格式字符串
+			String photo64 = Base64ImageUtil.byteArr2String(result.getYyzz());
+			result.setYyzzBase64(photo64);
+			resultVO.setResult(result);
+		}catch(Exception e){
+			logger.error("{}",e.getMessage());
+			resultVO.setCode(EConstants.CODE.FAILURE);
+		}
+		return resultVO;
+	}
 }
