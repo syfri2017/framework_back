8e6233fe11d9d40e68e03924fca3ec6a40ce1991
lixue0919 <442019696@qq.com>
2018-10-09 11:22:55 +0800

    展商管理查询Controller

--- exhibition/src/main/java/com/syfri/exhibition/controller/prediction/QyjbxxController.java
+++ exhibition/src/main/java/com/syfri/exhibition/controller/prediction/QyjbxxController.java
@@ -2,6 +2,7 @@ package com.syfri.exhibition.controller.prediction;
 
 import com.syfri.baseapi.model.ResultVO;
 import com.syfri.baseapi.utils.EConstants;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import com.syfri.exhibition.utils.Base64ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,4 +46,23 @@ public class QyjbxxController  extends BaseController<QyjbxxVO>{
 		}
 		return resultVO;
 	}
+
+    /**
+     * @Description: 根据用户信息、公司名称获取企业基本信息
+     * @Author: li.xue
+	 * @Date: 2018/10/9 11:05
+	 */
+    @ApiOperation(value="根据企业查询企业信息",notes="列表")
+    @ApiImplicitParam(name="vo",value="企业对象")
+    @PostMapping("/doFindZsxxByQyjbxx")
+    public @ResponseBody ResultVO doFindZsxxByQyjbxx(@RequestBody QyjbxxVO qyjbxxVO){
+        ResultVO resultVO = ResultVO.build();
+        try{
+            resultVO.setResult(qyjbxxService.doFindZsxxByQyjbxx(qyjbxxVO));
+        }catch(Exception e){
+            logger.error("{}",e.getMessage());
+            resultVO.setCode(EConstants.CODE.FAILURE);
+        }
+        return resultVO;
+    }
 }
