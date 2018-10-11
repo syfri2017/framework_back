143d4bb434b472beaec29cb7334679156fa66dd4
rliu666 <rliu666@sina.cn>
2018-10-11 13:30:06 +0800

    企业id查询

--- exhibition/src/main/java/com/syfri/exhibition/controller/prediction/QycpjsController.java
+++ exhibition/src/main/java/com/syfri/exhibition/controller/prediction/QycpjsController.java
@@ -1,24 +1,53 @@
 package com.syfri.exhibition.controller.prediction;
 
+import com.syfri.baseapi.model.ResultVO;
+import com.syfri.baseapi.utils.EConstants;
+import com.syfri.exhibition.utils.Base64ImageUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.syfri.exhibition.model.prediction.QycpjsVO;
 import com.syfri.exhibition.service.prediction.QycpjsService;
 import com.syfri.baseapi.controller.BaseController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("qycpjs")
-public class QycpjsController  extends BaseController<QycpjsVO>{
+public class QycpjsController extends BaseController<QycpjsVO> {
 
-	@Autowired
-	private QycpjsService qycpjsService;
+    @Autowired
+    private QycpjsService qycpjsService;
 
-	@Override
-	public QycpjsService getBaseService() {
-		return this.qycpjsService;
-	}
+    @Override
+    public QycpjsService getBaseService() {
+        return this.qycpjsService;
+    }
+
+    /**
+     * @Description: 根据企业id获取产品信息
+     * @Author: rliu
+     * @Date: 2018/10/9 10:35
+     */
+    @ApiOperation(value = "根据企业id获取产品信息", notes = "vo")
+    @RequestMapping("/doFindCpxxById")
+    public @ResponseBody ResultVO doFindCpxxById(@RequestBody QycpjsVO qycpjsVO) {
+        ResultVO resultVO = ResultVO.build();
+        try {
+            List<QycpjsVO> result = qycpjsService.doSearchListByVO(qycpjsVO);
+            for(QycpjsVO vo:result) {
+                //将二进制转为Base64格式字符串
+                String photo64 = Base64ImageUtil.byteArr2String(vo.getCptp());
+                vo.setCptpBase64(photo64);
+            }
+            resultVO.setResult(result);
+        } catch (Exception e) {
+            logger.error("{}", e.getMessage());
+            resultVO.setCode(EConstants.CODE.FAILURE);
+        }
+        return resultVO;
+    }
 
 }
--- exhibition/src/main/java/com/syfri/exhibition/controller/prediction/QyjsController.java
+++ exhibition/src/main/java/com/syfri/exhibition/controller/prediction/QyjsController.java
@@ -1,7 +1,13 @@
 package com.syfri.exhibition.controller.prediction;
 
+import com.syfri.baseapi.model.ResultVO;
+import com.syfri.baseapi.utils.EConstants;
+import com.syfri.exhibition.utils.Base64ImageUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,14 +17,38 @@ import com.syfri.baseapi.controller.BaseController;
 
 @RestController
 @RequestMapping("qyjs")
-public class QyjsController  extends BaseController<QyjsVO>{
+public class QyjsController extends BaseController<QyjsVO> {
 
-	@Autowired
-	private QyjsService qyjsService;
+    @Autowired
+    private QyjsService qyjsService;
 
-	@Override
-	public QyjsService getBaseService() {
-		return this.qyjsService;
-	}
+    @Override
+    public QyjsService getBaseService() {
+        return this.qyjsService;
+    }
+
+    /**
+     * @Description: 根据企业id获取产品信息
+     * @Author: rliu
+     * @Date: 2018/10/9 10:35
+     */
+    @ApiOperation(value = "根据企业id获取产品信息", notes = "vo")
+    @RequestMapping("/doFindQyjsById/{qyid}")
+    public @ResponseBody ResultVO doFindQyjsById(@PathVariable String qyid) {
+        ResultVO resultVO = ResultVO.build();
+        try {
+            QyjsVO result = qyjsService.doFindById(qyid);
+            if (result != null) {
+                //将二进制转为Base64格式字符串
+                String photo64 = Base64ImageUtil.byteArr2String(result.getLogo());
+                result.setLogoBase64(photo64);
+            }
+            resultVO.setResult(result);
+        } catch (Exception e) {
+            logger.error("{}", e.getMessage());
+            resultVO.setCode(EConstants.CODE.FAILURE);
+        }
+        return resultVO;
+    }
 
 }
